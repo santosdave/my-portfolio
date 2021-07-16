7e5369b589859a4e910b0a648ef53be9e7328d8a
santos dave <santosdave86@gmail.com>
2021-07-16 10:33:33 +0300

    second update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,16 +37,16 @@ function Contact() {
                     marginHeight={0}
                     marginWidth={0}
                     style={{ filter: "opacity(0.7)" }}
-                    src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+                    src="https://maps.google.com/maps?q=zetech%20university,ruiru,kiambu&t=&z=13&ie=UTF8&iwloc=&output=embed"
                 />
                 <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-                    <div className="lg:w-1/2 px-6">
+                    <div className="lg:w-1/2.5 px-6">
                     <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                         ADDRESS
                     </h2>
                     <p className="mt-1">
-                        97 Warren St. <br />
-                        New York, NY 10007
+                        Zetech University <br />
+                        Ruiru,Kiambu
                     </p>
                     </div>
                     <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -54,12 +54,12 @@ function Contact() {
                         EMAIL
                     </h2>
                     <a className="text-indigo-400 leading-relaxed">
-                        reedbarger@email.com
+                        santosdave86@gmail.com
                     </a>
                     <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                         PHONE
                     </h2>
-                    <p className="leading-relaxed">123-456-7890</p>
+                    <p className="leading-relaxed">0720880677</p>
                     </div>
                 </div>
                 </div>
--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Twitter Clone",
+    subtitle: "React and Firebase",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-1.gif",
     link: "https://reactbootcamp.com",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Amazon Clone",
+    subtitle: "React and Firebase",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-2.gif",
     link: "https://reactbootcamp.com",
   },
   {
-    title: "DevChat",
+    title: "Baze",
     subtitle: "React and Firebase",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
@@ -57,4 +57,7 @@ export const skills = [
   "Node",
   "GraphQL",
   "Material UI",
+  "Php",
+  "Python",
+  "Mysql",
 ];
